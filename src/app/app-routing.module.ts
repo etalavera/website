9ec787736af8eec4b428254b69681b795293b950
Enrique Talavera <etalavera.ordaz@gmail.com>
2018-09-17 22:23:37 -0500

    add servicios and contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './navigation/home/home.component';
 import { NosotrosComponent } from './navigation/nosotros/nosotros.component';
+import { ServiciosComponent } from './navigation/servicios/servicios.component';
+import { ContactoComponent } from './navigation/contacto/contacto.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'nosotros', component: NosotrosComponent}
+  {path: 'nosotros', component: NosotrosComponent},
+  {path: 'servicios', component: ServiciosComponent},
+  {path: 'contacto', component: ContactoComponent}
 ];
 
 @NgModule({
--- src/app/navigation/navigation.module.ts
+++ src/app/navigation/navigation.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { NosotrosComponent } from './nosotros/nosotros.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [HomeComponent, NosotrosComponent],
-  exports: [HomeComponent, NosotrosComponent]
+  declarations: [HomeComponent, NosotrosComponent, ServiciosComponent, ContactoComponent],
+  exports: [HomeComponent, NosotrosComponent, ServiciosComponent, ContactoComponent]
 })
 export class NavigationModule { }
